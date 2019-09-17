9a24cb91f0042da1829a62130177e46e70c69137
tojo423 <tayabjamil423@gmail.com>
2019-09-17 17:24:07 +0500

    Update SV_JailPlayer.lua

--- Jail/SV_JailPlayer.lua
+++ Jail/SV_JailPlayer.lua
@@ -7,12 +7,23 @@
 		-place this script under the ServerScript service
 	]]
 
+local jailAnim;
+local jailDelay_s = 3;
+local jailPos = CFrame.new(0, 0, 0);
+
 game.ReplicatedStorage.jailFunc.OnServerInvoke = function(player, targetChar)
-	if not targetChar then
-		return;
-	end
+	local humanoid = targetChar:FindFirstChildOfClass("Humanoid");
 	
-	-- do player animation
-	-- disable player controls
-	-- teleport player
+	local anim = humanoid:FindFirstChildOfClass("Animator");
+	local animTrack = anim:LoadAnimation(jailAnim);
+	animTrack:Play();
+	
+	local targetPlayer = game.Players:GetPlayerFromCharacter(targetChar);
+	local playerMod = require(targetPlayer.PlayerScripts:WaitForChild("PlayerModule"));
+	local ctrls = playerMod:GetControls();
+	ctrls:Disable();
+
+	wait(jailDelay_s);
+	humanoid.Torso.CFrame = jailPos;
+	ctrls:Enable();
 end
